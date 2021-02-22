3ebf69a9bbb08ca580733578a7ec6adb84217e8a
yue <yuewenduo@gmail.com>
2021-02-22 10:19:32 -0500

    Travel and Manipulation

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float speed = 3.0F;
+    public float rotateSpeed = 3.0F;
+    public float gravity = 10.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    CharacterController controller;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * curSpeed);
+    }
+}
---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    protected bool isPointEnter = false;
+    protected bool isPointDown = false;
+    public void PointEnter()
+    {
+        isPointEnter = true;
+    }
+
+    public void PointExit()
+    {
+        isPointEnter = false;
+    }
+
+    public void PointUp()
+    {
+        isPointDown = false;
+    }
+    
+    public void PointDown()
+    {
+        isPointDown = true;
+    }
+}
