a0a1aad3f38c04c435fb90204dd0ca8d25a1846b
yue <yuewenduo@gmail.com>
2021-02-22 19:05:57 -0500

    move & color

---
+++ Assets/Scripts/CubeColorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeColorController : MonoBehaviour
+{
+    private bool color = false;
+    private Renderer renderer;
+    private Color ORIGIN_COLOR;
+
+    void Start()
+    {
+        renderer = this.GetComponent<Renderer>();
+        ORIGIN_COLOR = renderer.material.GetColor("_Color");
+    }
+
+    public void changeColor()
+    {
+        color = !color;
+        renderer.material.SetColor("_Color", color ? Color.red : ORIGIN_COLOR);
+    }
+}
---
+++ Assets/Scripts/CubeMoveController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMoveController : CubeController
+{
+    private float speed = 2.0F;
+
+    void Update()
+    {
+        if (isPointEnter && isPointDown)
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
